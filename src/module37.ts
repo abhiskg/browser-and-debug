ac2de54c899fe6dccad7700606f3193f3c4efa4c
abhishek <abhiskg20@gmail.com>
2022-09-05 13:15:10 +0600

    setInterval with ++variable

---
+++ src/module37.ts
@@ -0,0 +1,14 @@
+// setInterval
+console.log("one");
+console.log("two");
+
+let second = 0;
+const intervalId = setInterval(() => {
+  //   second++;
+  console.log(++second); // 0 1 2 3 4 5
+  if (second === 5) {
+    clearInterval(intervalId);
+  }
+}, 1500);
+
+console.log("four");
