03c31a06d4dbc8bef57b001c522db2ab6d8c04af
abhishek <abhiskg20@gmail.com>
2022-09-08 12:38:32 +0600

    Array type data loaded and retrived from localstorage

--- js/module37.js
+++ js/module37.js
@@ -21,3 +21,60 @@ const getUserInfo = () => {
     const name = prompt("Tell your name");
     console.log(name);
 };
+// lOcation info
+const locationInfo = () => {
+    console.log("origin", location.origin);
+    console.log(location.port);
+};
+// GetCookies by name
+const getCookiesByName = (name) => {
+    const cookiesString = document.cookie;
+    const cookieParts = cookiesString.split("; ");
+    const cookie = cookieParts.find((c) => c.includes(name));
+    if (cookie) {
+        const cookieValue = cookie.split("=")[1];
+        return cookieValue;
+    }
+};
+localStorage.setItem("age", "40");
+console.log(localStorage.getItem("age"));
+// Set Storage data
+const setStorageData = () => {
+    const storageField = document.querySelector("#storage-field");
+    const fieldValue = storageField.value;
+    const previousData = localStorage.getItem("Type");
+    if (!previousData) {
+        const finalValue = [
+            {
+                type: fieldValue,
+            },
+        ];
+        localStorage.setItem("Type", JSON.stringify(finalValue));
+    }
+    else {
+        const prevUpdatedData = JSON.parse(previousData);
+        const finalValue = [
+            ...prevUpdatedData,
+            {
+                type: fieldValue,
+            },
+        ];
+        localStorage.setItem("Type", JSON.stringify(finalValue));
+    }
+    showStorageData();
+};
+const showStorageData = () => {
+    const storageContainer = document.querySelector("#storage-container");
+    storageContainer.textContent = "";
+    const storageDataString = localStorage.getItem("Type");
+    if (!storageDataString) {
+        return;
+    }
+    const storageData = JSON.parse(storageDataString);
+    storageData.forEach((data) => {
+        const li = document.createElement("li");
+        li.innerText = data.type;
+        storageContainer.append(li);
+    });
+};
+showStorageData();
